c49a665d815ae9a48f242453ba47c9a87b928ae9
Samartha Gs <119740787+Samartha0007@users.noreply.github.com>
2024-04-17 18:55:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -308,8 +308,8 @@ var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 
 //Same as above, but with port
 //If you want to load on localhost onto a different port, change 4000 to whatever port you please
-var port = process.env.OPENSHIFT_NODEJS_PORT || 4000;
+var port = process.env.OPENSHIFT_NODEJS_PORT || 1000;
 
 http.listen(port, ipaddress, function(){
-	console.log('Running on Openshift Server')
+	console.log('Samartha Server running')
 })
