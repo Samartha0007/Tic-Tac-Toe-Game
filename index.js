c6a0512cbd6a3249d17d392242689b2a0b504d01
Samartha Gs <119740787+Samartha0007@users.noreply.github.com>
2024-04-17 19:28:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -315,8 +315,8 @@ var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 
 //Same as above, but with port
 //If you want to load on localhost onto a different port, change 4000 to whatever port you please
-var port = process.env.PORT || 1000;
+var port = process.env.PORT || 4000;
 
 http.listen(port, ipaddress, function() {
-    console.log('Server.samartha running');
+    console.log('Server.samartha.cloud running');
 });
